910fff4ab617e9153ed006642f7e78f1c4c42250
Mhlengidlamini <127088435+Mhlengidlamini@users.noreply.github.com>
2024-01-17 11:33:12 -0800

    Add files via upload

---
+++ products.js
@@ -0,0 +1,156 @@
+export const productsData = [
+  {
+    id: 1,
+    title: "Tracksuit",
+    price: 700,
+    imageUrl: "./image/1.jpg",
+  },
+  {
+    id: 2,
+    title: "Short",
+    price: 300,
+    imageUrl: "./image/2.jpg",
+  },
+  {
+    id: 3,
+    title: "Short",
+    price: 300,
+    imageUrl: "./image/3.jpg",
+  },
+  {
+    id: 4,
+    title: "T-shirt",
+    price: 300,
+    imageUrl: "./image/4.jpg",
+  },
+  {
+    id: 5,
+    title: "HOOD",
+    price: 400,
+    imageUrl: "./image/5.jpg",
+  },
+
+  {
+    id: 9,
+    title: "Tempur Cloud Pillow",
+    price: 129,
+    imageUrl: "./image/9.jpg",
+  },
+  {
+    id: 10,
+    title: "Caco Pants ",
+    price: 450,
+    imageUrl: "./image/12.jpg",
+  
+  },
+  {
+    id: 11,
+    title: "Therapur Shallow Ottoman Base",
+    price: 700,
+    imageUrl: "./image/22.jpg",
+  },
+  {
+    id: 12,
+    title: "T-shirt",
+    price: 300,
+    imageUrl: "./image/IMG-20240115-WA0009.jpg",
+  },
+  {
+    id: 13,
+    title: "Kids Tracksuite",
+    price: 600,
+    imageUrl: "./image/IMG-20240115-WA0010.jpg",
+  },
+  
+  {
+    id: 14,
+    title: " Gilrs Set & Tracksuit",
+    price: 700,
+    imageUrl: "./image/IMG-20240115-WA0012.jpg",
+  },
+  {
+    id: 15,
+    title: " Mens Cloths",
+    price: 700,
+    imageUrl: "./image/IMG-20240115-WA0013.jpg",
+  },
+ 
+  
+  
+  {
+    id: 18,
+    title: "T-shirt",
+    price: 200,
+    imageUrl: "./image/IMG-20240115-WA0016.jpg",
+  },
+  
+  
+  {
+    id: 19,
+    title: "T-Shirt",
+    price: 250,
+    imageUrl: "./image/IMG-20240115-WA0017.jpg",
+  },
+  
+  {
+    id: 20,
+    title: "T-shirt",
+    price: 200,
+    imageUrl: "./image/IMG-20240115-WA0018.jpg",
+  },
+  
+  
+  {
+    id: 21,
+    title: "Hood",
+    price: 350,
+    imageUrl: "./image/IMG-20240115-WA0019.jpg",
+  },
+  
+  {
+    id: 22,
+    title: "Tracksuit",
+    price: 700,
+    imageUrl: "./image/IMG-20240115-WA0020.jpg",
+  },
+  
+  {
+    id: 23,
+    title: "Tracksuit",
+    price: 700,
+    imageUrl: "./image/IMG-20240115-WA0021.jpg",
+  },
+  
+  
+  {
+    id: 24,
+    title: "Sets",
+    price: 550,
+    imageUrl: "./image/IMG-20240115-WA0022.jpg",
+  },
+  
+  
+  {
+    id: 25,
+    title: "Short Pants",
+    price: 300,
+    imageUrl: "./image/IMG-20240115-WA0023.jpg",
+  },
+  
+  
+  
+ 
+  {
+    id: 29,
+    title: "Short Pantse",
+    price: 300,
+    imageUrl: "./image/IMG-20240115-WA0027.jpg",
+  }
+  
+  ,{
+    id: 30,
+    title: "Short Pants",
+    price: 300,
+    imageUrl: "./image/IMG-20240115-WA0028.jpg",
+  },
+];
